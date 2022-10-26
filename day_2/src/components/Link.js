5d246fa7e063e5ebc91d284a562c429a073e4779
Georgi <ggeorgeuk@gmail.com>
2022-10-26 09:01:05 +0100

    Add files via upload

---
+++ day_2/src/components/Link.js
@@ -0,0 +1,9 @@
+import React from "react";
+//props comming from parent(LinkedList)
+const Link = ({name}) => {
+    return (
+        <h1>{name}</h1>
+
+    )
+};
+export default Link;
---
+++ day_2/src/components/LinkList.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+import Link from "./Link";
+
+const LinkList = ({links}) => {
+    //dont have state so we dont use hook
+    //use props object(links) passed from parent component(LinkBox)
+
+    const linksNodes = links.map((link) => {
+        return (
+            //return how we want to look like one link...add it to new array
+            <Link name={link.name} key={link.id}/>
+        )
+    });
+    return (
+        <div>
+           
+           {/**return whole new list */}
+           {linksNodes}
+
+        </div>
+     
+    )
+
+}
+export default LinkList;
