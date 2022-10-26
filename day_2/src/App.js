91a119f31c984f7fde2da9e88146797ec0342763
Georgi <ggeorgeuk@gmail.com>
2022-10-26 08:58:30 +0100

    Add files via upload

---
+++ day_2/src/App.js
@@ -0,0 +1,14 @@
+import LinkBox from "./containers/LinkBox";
+
+
+function App() {
+  return (
+    <div>
+      {/**show whole container **/}
+      <LinkBox />
+    </div>
+   
+  );
+}
+
+export default App;
